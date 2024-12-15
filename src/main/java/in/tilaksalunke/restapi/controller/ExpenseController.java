90deb31ac8c3c75841c9ab7b48181e0f7e04097f
tilaksolunke <tilaksolunke2002@gmail.com>
2024-12-15 12:18:25 +0530

    completed the read expenses api

---
+++ src/main/java/in/tilaksalunke/restapi/controller/ExpenseController.java
@@ -0,0 +1,51 @@
+package in.tilaksalunke.restapi.controller;
+
+import in.tilaksalunke.restapi.dto.ExpenseDTO;
+import in.tilaksalunke.restapi.io.ExpenseResponse;
+import in.tilaksalunke.restapi.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * This is controller class for Expense module
+ * @author Tilak S
+ * */
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class ExpenseController {
+    private final ExpenseService expenseService;
+    private final ModelMapper modelMapper;
+
+    /**
+     * It will fetch the expenses from database
+     * @return list
+     * */
+    @GetMapping("/expenses")
+    public List<ExpenseResponse> getExpenses(){
+        log.info("API GET /expenses called");
+        // Call the service method
+        List<ExpenseDTO> list = expenseService.getAllExpenses();
+        log.info("Printing the data from service {}", list);
+
+        //Convert the expense DTO to expense response
+        List<ExpenseResponse> response = list.stream().map(expenseDTO -> mapToExpenseResponse(expenseDTO)).collect(Collectors.toList());
+        return response;
+    }
+
+    /**
+     * Mapper method for converting expense dto object to expense response
+     * @param expenseDTO
+     * @return ExpenseResponse
+     * */
+    private ExpenseResponse mapToExpenseResponse(ExpenseDTO expenseDTO) {
+        return modelMapper.map(expenseDTO, ExpenseResponse.class);
+    }
+}
---
+++ src/main/java/in/tilaksalunke/restapi/service/impl/ExpenseServiceImpl.java
@@ -0,0 +1,53 @@
+package in.tilaksalunke.restapi.service.impl;
+
+import in.tilaksalunke.restapi.dto.ExpenseDTO;
+import in.tilaksalunke.restapi.entity.ExpenseEntity;
+import in.tilaksalunke.restapi.repository.ExpenseRepository;
+import in.tilaksalunke.restapi.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Service implementation for expense module
+ * @author Tilak S
+ * */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+
+public class ExpenseServiceImpl implements ExpenseService {
+    private final ExpenseRepository expenseRepository;
+    private final ModelMapper modelMapper;
+
+    /**
+     * It will fetch the expense from database
+     * @return list
+     * */
+    @Override
+    public List<ExpenseDTO> getAllExpenses(){
+        //Call the repository method
+        List<ExpenseEntity> list = expenseRepository.findAll();
+        log.info("Printing the data from repository {}", list);
+
+        //Convert the entity object to DTO object
+        List<ExpenseDTO> listOfExpenses  =  list.stream().map(expenseEntity -> mapToExpenseDTO(expenseEntity)).collect(Collectors.toList());
+
+        //Return the list
+        return listOfExpenses;
+    }
+
+    /**
+     * Mapper method to convert expense entity to expense DTO
+     * @param expenseEntity
+     * @return ExpenseDTO
+     * */
+
+    private ExpenseDTO mapToExpenseDTO(ExpenseEntity expenseEntity) {
+        return modelMapper.map(expenseEntity, ExpenseDTO.class);
+    }
+}
